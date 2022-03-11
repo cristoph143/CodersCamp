28be07c65b764f42ad9cc7a9e3e8c048a90aedac
Cristopher Bohol <cristopher.bohol.21@usjr.edu.ph>
2022-03-11 17:13:22 +0800

    Part3

--- Intermediate Algorithm Scripting.js
+++ Intermediate Algorithm Scripting.js
@@ -27,6 +27,8 @@ function diffArray(arr1, arr2) {
     return newArr;
 }
 
+// Intermediate Algorithm Scripting: Arguments Optional
+
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]);
 diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]);
@@ -36,3 +38,86 @@ diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]);
 diffArray([], ["snuffleupagus", "cookie monster", "elmo"]);
 diffArray([1, "calf", 3, "piglet"], [7, "filly"]);
+
+function destroyer(arr) {
+    const args = Array.from(arguments);
+    args.shift();
+    return arr.filter(val => !args.includes(val));
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3);
+destroyer([3, 5, 1, 2, 2], 2, 3, 5);
+destroyer(["tree", "hamburger", 53], "tree", 53);
+destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan");
+
+// Intermediate Algorithm Scripting: Wherefore art thou
+function whatIsInAName(collection, source) {
+    const srcKeys = Object.keys(source);
+    return collection.filter(
+        obj => {
+            return srcKeys
+                .every(
+                    key =>
+                    obj.hasOwnProperty(key) &&
+                    obj[key] === source[key]
+                );
+        }
+    );
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+whatIsInAName([{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }], { "apple": 1 });
+whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 });
+whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 });
+whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2, "cookie": 2 });
+whatIsInAName([{ "a": 1, "b": 2, "c": 3 }], { "a": 1, "b": 9999, "c": 3 })
+
+// Intermediate Algorithm Scripting: Spinal Tap Case
+function spinalCase(str) {
+    return str.replace(
+        /([a-z])([A-Z])/g, '$1 $2'
+    ).replace(
+        /\s+|_+/g, '-'
+    ).toLowerCase();
+}
+spinalCase('This Is Spinal Tap');
+spinalCase('thisIsSpinalTap');
+spinalCase('The_Andy_Griffith_Show');
+spinalCase('Teletubbies say Eh-oh');
+spinalCase('AllThe-small Things');
+
+// Intermediate Algorithm Scripting: Pig Latin
+function translatePigLatin(str) {
+    return str
+        .replace(/^[aeiou]\w*/, "$&way")
+        .replace(/(^[^aeiou]+)(\w*)/, "$2$1ay");
+}
+translatePigLatin("california");
+translatePigLatin("paragraphs");
+translatePigLatin("consonant");
+translatePigLatin("glove");
+translatePigLatin("eight");
+translatePigLatin("algorithm");
+translatePigLatin("eighteen");
+translatePigLatin("rythm");
+translatePigLatin("schwartz");
+
+// Intermediate Algorithm Scripting: Search and Replace
+
+function myReplace(str, before, after) {
+    const firstLetter = before[0];
+    if (firstLetter === firstLetter.toUpperCase()) {
+        after = after.charAt(0).toUpperCase() + after.slice(1);
+    }
+    if (firstLetter === firstLetter.toLowerCase()) {
+        after = after.charAt(0).toLowerCase() + after.slice(1);
+    }
+    return str.replace(before, after);
+}
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+myReplace("He is Sleeping on the couch", "Sleeping", "sitting");
+myReplace("This has a spellngi error", "spellngi", "spelling");
+myReplace("His name is Tom", "Tom", "john");
+myReplace("Let us get back to more Coding", "Coding", "algorithms");
+myReplace("I think we should look up there", "up", "Down");
